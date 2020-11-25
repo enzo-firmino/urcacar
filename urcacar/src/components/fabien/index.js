29e3443e6702c06d486b722e243f4b616a2d5381
Fabien BOUDEVIN <fabien.boudevin@etudiant.univ-reims.fr>
2020-11-25 14:46:19 +0100

    Installation Leaflet

---
+++ urcacar/src/components/fabien/index.js
@@ -0,0 +1,22 @@
+import { Map, MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import React from 'react';
+
+function Test(){
+    const position = [51.505, -0.09]
+        
+    return (
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+            </Marker>
+        </MapContainer>
+    )
+}
+
+export default Test;
