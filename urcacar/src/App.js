138d0fcb6289e84dca38696693db532ea092f044
Enzo Firmino <firmino.enzo@hotmail.fr>
2020-11-27 12:14:59 +0100

    Creation Form search trajet

--- urcacar/src/App.js
+++ urcacar/src/App.js
@@ -1,7 +1,7 @@
-
 import './App.css';
 import 'leaflet/dist/leaflet.css'
 import {FormSearchTrajet} from "./components/FormSearchTrajet";
+import React from "react";
 
 function App() {
   return (
--- urcacar/src/components/FormSearchTrajet/index.js
+++ urcacar/src/components/FormSearchTrajet/index.js
@@ -2,7 +2,8 @@ import React, {useState} from "react";
 import Form from 'react-bootstrap/Form';
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-
+import { ArrowDownUp } from 'react-bootstrap-icons';
+import '../../styles/formSearchTrajet.css';
 
 export function FormSearchTrajet(props) {
 
@@ -14,9 +15,10 @@ export function FormSearchTrajet(props) {
 
     let currentDate = new Date();
 
+
     const [depart, setDepart] = useState('');
     const [arrive, setArrive] = useState('');
-    const [date, setDate] = useState(currentDate.getDate());
+    const [date, setDate] = useState(Date());
     const [heureDepart, setHeureDepart] = useState(currentDate.getHours());
     const [heureArrive, setHeureArrive] = useState(currentDate.getHours());
     const [nbPassager, setNbPassager] = useState(1);
@@ -26,84 +28,83 @@ export function FormSearchTrajet(props) {
         <Form>
 
             <Form.Group as={Col} controlId="formGridDepart">
-                <Form.Control type="email" placeholder="Depart" />
+                <Form.Control
+                    className='fullBgField'
+                    type="text"
+                    placeholder="Depart"
+                    value={depart}
+                    onChange={e => setDepart(e.target.value)}/>
             </Form.Group>
 
+            <Button
+                variant="outline-success"
+                onClick={ () => {
+                    let temp = depart;
+                    setDepart(arrive);
+                    setArrive(temp);
+                }}
+            >
+                <ArrowDownUp/>
+            </Button>
+
             <Form.Group as={Col} controlId="formGridArrive">
-                <Form.Control type="password" placeholder="Arrivée" />
+                <Form.Control
+                    className="fullBgField"
+                    type="text"
+                    placeholder="Arrivée"
+                    value={arrive}
+                    onChange={e => setArrive(e.target.value)}/>
             </Form.Group>
 
             <Form.Row>
                 <Form.Group as={Col} controlId="formGridDate">
-                    <Form.Control type="date"  />
+                    <Form.Control
+                        className="emptyBgField"
+                        type="date"
+                        placeholder="Date"
+                        value={date}
+                        onChange={e => setDate(e.target.value)}/>
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridHeureDepart">
-                    <Form.Control type="time"  />
+                    <Form.Control
+                        className="emptyBgField"
+                        type="time"
+                        placeholder="Heure départ"
+                        value={heureDepart}
+                        onChange={e => setHeureDepart(e.target.value)}/>
                 </Form.Group>
             </Form.Row>
 
 
             <Form.Row>
                 <Form.Group as={Col} controlId="formGridHeureArrive">
-                    <Form.Control type="time"  />
+                    <Form.Control
+                        className="emptyBgField"
+                        type="time"
+                        placeholder="Heure arrivée"
+                        value={heureArrive}
+                        onChange={e => setHeureArrive(e.target.value)}/>
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridNbPassagers">
-                    <Form.Control type="range"  />
+                    <Form.Control
+                        className="emptyBgField"
+                        type="number"
+                        min="1"
+                        max="5"
+                        placeholder="Nombre passagers"
+                        value={nbPassager}
+                        onChange={e => setNbPassager(parseInt(e.target.value))}/>
                 </Form.Group>
             </Form.Row>
 
-            <Button variant="primary" type="submit">
+            <Button
+                className='fullBgField'
+                variant="primary"
+                type="submit">
                 Rechercher
             </Button>
         </Form>);
-
-    //     <form onSubmit={handleSubmit}>
-    //     <label>
-    //         <input
-    //             placeholder='Depart'
-    //             type="text"
-    //             value={depart}
-    //             onChange={e => setDepart(e.target.value)}
-    //         />
-    //     </label>
-    //     <label>
-    //         <input
-    //             placeholder='Arrivée'
-    //             type="text"
-    //             value={arrive}
-    //             onChange={e => setArrive(e.target.value)}
-    //         />
-    //     </label>
-    //     <label>
-    //         <input
-    //             type="date"
-    //             value={date}
-    //             onChange={e => setDate(e.target.value)}
-    //         />
-    //     </label>
-    //     <label>
-    //         <input
-    //             type="time"
-    //             value={heureDepart}
-    //             onChange={e => setHeureDepart(e.target.value)}
-    //         />
-    //     </label>
-    //     <label>
-    //         <input
-    //             type="time"
-    //             value={heureArrive}
-    //             onChange={e => setHeureArrive(e.target.value)}
-    //         />
-    //     </label>
-    //     <label>
-    //         <input
-    //             type="range"
-    //             value={nbPassager}
-    //             onChange={e => setNbPassager(e.target.value)}
-    //         />
-    //     </label>
-    //     <input type="submit" value="Envoyer"/>
-    // </form>)
 }
+
