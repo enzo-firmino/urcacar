174ab65c97d1a6807805990b2fc3e21b564d6d12
Mathis <mathis@mathis.com>
2020-11-30 16:15:57 +0100

    Trajet

---
+++ trajet/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import 'leaflet/dist/leaflet.css'
+import {FormSearchTrajet} from "./components/FormSearchTrajet";
+import Test from "./components/fabien/index";
+
+function App() {
+  return (
+    <div className="App">
+      <FormSearchTrajet />
+        <Test/>
+
+    </div>
+
+  );
+}
+
+export default App;
