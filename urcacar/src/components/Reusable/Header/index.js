ef0b650c5dde497168e4293c70fea85a5b129810
Fabien BOUDEVIN <fabien.boudevin@etudiant.univ-reims.fr>
2020-12-02 12:09:53 +0100

    Modification du Header

--- urcacar/src/components/Reusable/Header/index.js
+++ urcacar/src/components/Reusable/Header/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, Image } from "react-bootstrap";
+import { Bell, Envelope } from "react-bootstrap-icons";
+import profilePicture from '../../../assets/profilepicture.jpg';
 import {
     BrowserRouter as Router,
     Switch,
@@ -10,13 +12,15 @@ import {
 export function Header(props) {
 
     return (
+        
             <Navbar expand="lg" bg="success" variant="dark">
                 <div className="container">
                     <Link className="aW" to="/">UrcaCar</Link>
-                    <Nav className="d-flex flex-fill justify-content-around">
-                        <Link className="aW" to="/messagerie">Messagerie</Link>
-                        <Link className="aW" to="/profil">Profil</Link>
-                        <Link className="aW" to="/notifications">Notifications</Link>
+                    <Nav className="d-flex flex-fill justify-content-end">
+                    
+                        <Link className="aW" to="/messagerie"><Envelope size={35}/></Link>
+                        <Link className="aW" to="/profil"><Image style={{height:35, width:35, marginRight:10, marginLeft:10}} src={profilePicture} roundedCircle/></Link>
+                        <Link className="aW" to="/notifications"><Bell size={35}/></Link>
                     </Nav>
                 </div>
             </Navbar>
