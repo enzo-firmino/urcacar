dfc625ecc68dc9c0eea56098618871a321e62ea5
Enzo Firmino <firmino.enzo@hotmail.fr>
2021-01-25 13:01:25 +0100

    USerFixtures

--- API/UcAPI/src/DataFixtures/UserFixtures.php
+++ API/UcAPI/src/DataFixtures/UserFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
 use App\Entity\Utilisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -17,7 +16,24 @@ class UserFixtures extends Fixture
 
         for($nbUsers = 1; $nbUsers <= 10; $nbUsers++){
             $user = new Utilisateur();
-            $user->setPnom($faker->firstName);
+            $user->setPnomuser($faker->firstName);
+            $user->setEmailuser($faker->email);
+            $user->setTeluser($faker->phoneNumber);
+            $user->setStatutuser($faker->numberBetween(0, 1));
+            $user->setRang($faker->numberBetween(0, 2));
+            $user->setFiabilite($faker->numberBetween(0, 5));
+            $user->setDemanderes($faker->boolean);
+            $user->setResaccepter($faker->boolean);
+            $user->setResenvoyer($faker->boolean);
+            $user->setDemandenota($faker->boolean);
+            $user->setTrajetannuler($faker->boolean);
+            $user->setTrajetimi($faker->boolean);
+            $user->setNouvelleavis($faker->boolean);
+            $user->setDialogue($faker->boolean);
+            $user->setFumer($faker->boolean);
+            $user->setMusique($faker->boolean);
+            $user->setPhotouser($faker->imageUrl($width = 350, $height = 350, 'people'));
+
             $manager->persist($user);
         }
         $manager->flush();
